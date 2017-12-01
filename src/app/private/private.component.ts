0e8deb78e754a02d6d168105b0ac111b8c04e8cf
Stéphane Nabhan <Stphane Nabhan>
2017-12-01 15:02:37 +0100

    Ajout number avec autocompletition et test

--- src/app/private/private.component.ts
+++ src/app/private/private.component.ts
@@ -1,12 +1,12 @@
 import {Component} from '@angular/core';
 import {FormControl} from '@angular/forms';
-
 import {Observable} from 'rxjs/Observable';
 import {map} from 'rxjs/operators/map';
 
+
 export class State {
   constructor(public name: string, public firstName: string, public job: string,
-              public picture: string) {
+              public picture: string, public number: [string]) {
   }
 }
 
@@ -19,7 +19,8 @@ export class State {
 export class PrivateComponent {
   selectedJob: string;
   filteredUser: {};
-  showResults: Boolean = false;
+  auCompletedNumList: Observable<any[]>;
+  numInputCtrl: FormControl;
   nameInputCtrl: FormControl;
   selectCtrl: FormControl;
   auCompletedList: Observable<any[]>;
@@ -33,12 +34,14 @@ export class PrivateComponent {
   states: State[] = [
     {
       name: 'Basson',
+      number: ['06', '80', '98', '56', '25'],
       firstName: 'Julien',
       picture: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Flag_of_Arkansas.svg',
       job: 'CTO'
     },
     {
       name: 'Caminale',
+      number: ['06', '75', '18', '34', '72'],
       firstName: 'Loic',
       picture: 'https://upload.wikimedia.org/wikipedia/commons/0/01/Flag_of_California.svg',
       job: 'HR'
@@ -46,12 +49,14 @@ export class PrivateComponent {
     {
       name: 'Nabhan',
       firstName: 'Stéphane',
+      number: ['06', '17', '42', '56', '85'],
       picture: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Florida.svg',
       job: 'CEO'
     },
     {
       name: 'Ollier',
       firstName: 'Thomas',
+      number: ['06', '53', '98', '85', '76'],
       picture: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Texas.svg',
       job: 'HR'
     }
@@ -60,6 +65,12 @@ export class PrivateComponent {
   constructor() {
     this.nameInputCtrl = new FormControl();
     this.selectCtrl = new FormControl();
+    this.numInputCtrl = new FormControl();
+
+    this.auCompletedNumList = this.numInputCtrl.valueChanges
+      .pipe(
+        map((state: string) => state ? this.filteringDataByNum(state, this.states) : null)
+      );
     this.auCompletedList = this.nameInputCtrl.valueChanges
       .pipe(
         map((state: string) => state ? this.filteringDataByName(state, this.states) : null)
@@ -73,12 +84,23 @@ export class PrivateComponent {
   }
 
   filteringDataByName(dataEnter: string, data) {
-    const returnValue = data.filter(state =>
+    return (data.filter(state =>
       (state.name + ' ' + state.firstName).toLowerCase().indexOf(dataEnter.toLowerCase()) === 0 ||
-      (state.firstName + ' ' + state.name).toLowerCase().indexOf(dataEnter.toLowerCase()) === 0);
-    console.log(returnValue);
-    this.filteredUser = returnValue;
-    return (returnValue);
+      (state.firstName + ' ' + state.name).toLowerCase().indexOf(dataEnter.toLowerCase()) === 0));
+  }
+
+  filteringDataByNum(dataEnter: string, data) {
+    return (data.filter(state =>
+      ( state.number[0] + ' ' +
+        state.number[1] + ' ' +
+        state.number[2] + ' ' +
+        state.number[3] + ' ' +
+        state.number[4]).indexOf(dataEnter.toLowerCase()) === 0 ||
+      ( state.number[0] +
+        state.number[1] +
+        state.number[2] +
+        state.number[3] +
+        state.number[4] ).indexOf(dataEnter.toLowerCase()) === 0));
   }
 
   onClickSearch() {
@@ -88,8 +110,14 @@ export class PrivateComponent {
     }else {
       Result = this.states;
     }
-    if (this.nameInputCtrl.value.length !== 0) {
+    if (this.nameInputCtrl.value !== null) {
       Result = this.filteringDataByName(this.nameInputCtrl.value, Result);
+    }
+
+    console.log(this.numInputCtrl.value);
+
+    if (this.numInputCtrl.value !== null) {
+      Result = this.filteringDataByNum(this.numInputCtrl.value, Result);
     }
     this.coworkerListFiltering = Result;
     console.log('ONCLICKSEARCH ' + JSON.stringify(Result, null, 2));
